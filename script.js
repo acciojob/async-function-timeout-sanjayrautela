25a1104dd847431c0ce68e454937febebd3e4751
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-02-23 14:22:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.getElementById('btn').addEventListener('click', async function() {
+	const text = document.getElementById('text').value;
+	const delay = document.getElementById('delay').value;
+
+	await new Promise(resolve => setTimeout(resolve,delay));
+
+	document.getElementById('output').innerText = text;
+});
+
